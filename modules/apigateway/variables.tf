ca55977a2519c84ad81a05517afe4a6095d21dd4
Dennis Amankwa <amankwa32@hotmail.com>
2025-12-15 18:31:12 +0000

    Create variables.tf

---
+++ modules/apigateway/variables.tf
@@ -0,0 +1,21 @@
+variable "env" {
+  description = "Deployment environment"
+  type        = string
+}
+
+variable "lambda_invoke_arn" {
+  description = "ARN of the Lambda function to integrate"
+  type        = string
+}
+
+variable "lambda_name" {
+  description = "Name of the Lambda function"
+  type        = string
+}
+
+variable "prefix" {
+  type        = string
+}
+variable "api_gateway_name" {
+  type        = string
+}
