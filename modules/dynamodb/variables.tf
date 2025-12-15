d037f9e9ac9ee88df3a91ad2c57353a429db673f
Dennis Amankwa <amankwa32@hotmail.com>
2025-12-15 18:42:52 +0000

    Create variables.tf

---
+++ modules/dynamodb/variables.tf
@@ -0,0 +1,12 @@
+variable "env" {
+  description = "Deployment environment"
+  type        = string
+}
+
+variable "table_name_suffix" {
+  description = "Optional suffix for the table name"
+  type        = string
+}
+variable "prefix" {
+  type        = string
+}
